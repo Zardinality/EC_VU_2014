6376647cc2d6c93f3c547b6cdff1f2676cb18529
MindExodus <MindExodus@gmail.com>
2014-09-13 16:41:14 +0200

    minor change of Fletcher

--- src/FletcherEvaluation.java
+++ src/FletcherEvaluation.java
@@ -17,45 +17,56 @@ public class FletcherEvaluation implements ContestEvaluation{
 	private String regular_ = "false";
 	private String separable_ = "false";
 	private String evals_ = Integer.toString(EVALS_LIMIT_);
+	private Random rnd_;
+	double[] alpha_;
+	int[][] a_;
+	int[][] b_;
 	
 	public FletcherEvaluation()
 	{
-		best_ = 100;
-		evaluations_ =0;
-	}
-	
-	private double func(double[] x)
-	{
-		double f=0;
-		Random rnd = new Random();
+		best_ = -1000000;
+		evaluations_ = 0;
+		rnd_ = new Random();
+		rnd_.setSeed(0);
+		alpha_ = new double[10];
+		a_ = new int[10][10];
+		b_ = new int[10][10];
 		for(int i=0;i<10;i++)
 		{
-			int a = rnd.nextInt(200)-100;
-			int b = rnd.nextInt(200)-100;
-			double alpha = rnd.nextDouble()*2*Math.PI-Math.PI;
-			
-			f += Math.pow((func_A(a,b,alpha) - func_B(a,b,x[i])), 2);
-			
+			alpha_[i] = rnd_.nextDouble()*2*Math.PI-Math.PI;
+			for(int j=0;j<10;j++)
+			{
+				a_[i][j] = rnd_.nextInt(201)-100;
+				b_[i][j] = rnd_.nextInt(201)-100;
+			}
+		}
+	}
+	
+	private double func(double[] x, double[] alpha, int[][] a, int[][] b)
+	{
+		double f = 0;
+		for(int i=0;i<10;i++)
+		{
+			f += Math.pow((func_A(a[i],b[i],alpha) - func_B(a[i],b[i],x)), 2);
 		}
 		return f;
 	}
-	private double func_A(int a,int b,double alpha)
+	private double func_A(int[] a,int[] b,double[] alpha)
 	{
-		
 		double sum = 0;
 		for(int j = 0;j<10;j++)
 		{	
-			sum += (a*Math.sin(alpha) + b*Math.cos(alpha));
+			sum += (a[j]*Math.sin(alpha[j]) + b[j]*Math.cos(alpha[j]));
 		}
 		return sum;
 	}
 	
-	private double func_B(int a,int b,double x)
+	private double func_B(int[] a,int[] b,double[] x)
 	{
 		double sum = 0;
 		for(int j = 0;j<10;j++)
 		{
-			sum += (a*Math.sin(x/5*Math.PI) + b*Math.cos(x/5*Math.PI));
+			sum += (a[j]*Math.sin(x[j]/5*Math.PI) + b[j]*Math.cos(x[j]/5*Math.PI));
 		}
 		return sum;
 	}
@@ -69,9 +80,9 @@ public class FletcherEvaluation implements ContestEvaluation{
 		if(ind.length!=10) throw new IllegalArgumentException();
 			
 		if(evaluations_>EVALS_LIMIT_) return null;
-		
+
 		double f = 0;
-		f = 10 - func(ind);
+		f = 10 - func(ind,alpha_,a_,b_);
 		if(f>best_) best_ = f;
 		evaluations_++;
 		
