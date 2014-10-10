00c6d2cf633ee091c4cffa09b4c317599dbb5d6c
MindExodus <MindExodus@gmail.com>
2014-10-10 21:19:42 +0200

    lambda = 40, sigma = 2

--- src/player19.java
+++ src/player19.java
@@ -401,12 +401,14 @@ public class player19 implements ContestSubmission {
 	}
 
 	private void CMA_ES_RS() {
-		int lambda = 10;
-		double sigma = 3;
+		int lambda_default = 40;
+		int lambda = lambda_default;
+		double sigma_default = 2;
+		double sigma = sigma_default;
 		while (limit_ * 2 > (int) lambda
 				* (100 + 50 * Math.pow((DIM + 3), 2) / Math.sqrt(lambda))) {
-			CMA_ES(lambda,sigma);
-			lambda = (int)Math.round(lambda * 1.1);
+			CMA_ES(lambda, sigma);
+			//lambda = (int) Math.round(lambda * 1.1);
 		}
 	}
 
